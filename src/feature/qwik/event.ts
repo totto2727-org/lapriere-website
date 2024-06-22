4035bcd6575c5d8d8f3069336876c54f50c61798
totto2727 <kaihatu.totto2727@gmail.com>
2024-06-23 02:20:04 +0900

    improve: rename event

--- src/feature/qwik/event.ts
+++ src/feature/qwik/event.ts
@@ -1,16 +1,19 @@
-export interface EventWithName {
-  name: string
+export interface EventWithName<T extends string, U extends string> {
+  eventName: T
+  astroEventName: U
   create: () => CustomEvent
 }
 
-const QWIK_PAGE_LOAD_EVENT_NAME = 'qwik:page-load'
-export const qwikPageLoadEvent: EventWithName = {
-  name: QWIK_PAGE_LOAD_EVENT_NAME,
+const QWIK_PAGE_LOAD_EVENT_NAME = 'qwikPageLoad'
+export const qwikPageLoadEvent: EventWithName<typeof QWIK_PAGE_LOAD_EVENT_NAME, 'astro:page-load'> = {
+  eventName: QWIK_PAGE_LOAD_EVENT_NAME,
+  astroEventName: 'astro:page-load',
   create: () => new CustomEvent(QWIK_PAGE_LOAD_EVENT_NAME),
 }
 
-const QWIK_AFTER_SWAP_EVENT_NAME = 'qwik:after-swap'
-export const qwikAfterSwapEvent: EventWithName = {
-  name: QWIK_AFTER_SWAP_EVENT_NAME,
-  create: () => new CustomEvent(QWIK_PAGE_LOAD_EVENT_NAME),
+const QWIK_AFTER_SWAP_EVENT_NAME = 'qwikAfterSwap'
+export const qwikAfterSwapEvent: EventWithName<typeof QWIK_AFTER_SWAP_EVENT_NAME, 'astro:after-swap'> = {
+  eventName: QWIK_AFTER_SWAP_EVENT_NAME,
+  astroEventName: 'astro:after-swap',
+  create: () => new CustomEvent(QWIK_AFTER_SWAP_EVENT_NAME),
 }
--- src/feature/qwik/hook.ts
+++ src/feature/qwik/hook.ts
@@ -2,6 +2,6 @@ import { useOnDocument } from '@builder.io/qwik'
 
 import { qwikAfterSwapEvent, qwikPageLoadEvent } from './event'
 
-export const usePageLoadTask = (eventQrl: Parameters<typeof useOnDocument>[1]) => useOnDocument(qwikPageLoadEvent.name, eventQrl)
 
-export const useAfterSwapTask = (eventQrl: Parameters<typeof useOnDocument>[1]) => useOnDocument(qwikAfterSwapEvent.name, eventQrl)
+export const usePageLoadTask = (eventQrl: Parameters<typeof useOnDocument>[1]) => useOnDocument(qwikPageLoadEvent.eventName, eventQrl)
+export const useAfterSwapTask = (eventQrl: Parameters<typeof useOnDocument>[1]) => useOnDocument(qwikAfterSwapEvent.eventName, eventQrl)
