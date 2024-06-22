a4316fd107e62b2085686e7dd6eb93551e355934
totto2727 <kaihatu.totto2727@gmail.com>
2024-06-23 04:48:45 +0900

    fix: fix: instanceof behavior differs in vite build only

--- src/feature/theme/lib/event.ts
+++ src/feature/theme/lib/event.ts
@@ -1,6 +1,9 @@
+import { is, partial } from 'valibot'
+
 import { saveColorThemeOnCookie } from './cookie.client'
 import { saveColorThemeOnDom } from './dom'
 import type { ColorTheme } from './type'
+import { colorThemeValidator } from './type'
 
 /**
  * テーマを変更するイベントのデフォルトのターゲットを取得する関数
@@ -13,6 +16,28 @@ export function getColorThemeChangeEventDefaultTarget(): HTMLElement {
 
 class ColorThemeChangeEvent extends CustomEvent<Partial<ColorTheme>> {
   static eventName = 'color-theme:change'
+
+  /**
+   * Vite利用時、開発環境と本番環境でinstanceofの結果が異なることがあるため緩衝策して実装している関数
+   *
+   * 検証結果に応じてパススルー、初期化等を切り替えます
+   *
+   * ColorThemeChangeEventに該当しないイベントオブジェクトを渡すと例外を返します
+   *
+   * @param e イベント
+   * @returns
+   */
+  static reconstructor(e: Event) {
+    if (e instanceof ColorThemeChangeEvent) {
+      return e
+    }
+
+    if (e instanceof CustomEvent && is(partial(colorThemeValidator), e.detail)) {
+      return new ColorThemeChangeEvent(e.detail)
+    }
+
+    throw new Error('不適切なイベントオブジェクトが渡されています')
+  }
 
   constructor(theme: Partial<ColorTheme>) {
     super(ColorThemeChangeEvent.eventName, { detail: theme })
@@ -72,10 +97,8 @@ export function dispatchColorThemeChange(
  */
 export function addColorThemeChangeEventListener(targetElement: HTMLElement): void {
   targetElement.addEventListener(ColorThemeChangeEvent.eventName, (e) => {
-    if (!(e instanceof ColorThemeChangeEvent)) {
-      return
-    }
+    const event = ColorThemeChangeEvent.reconstructor(e)
 
-    onColorThemeChange(e, targetElement)
+    onColorThemeChange(event, targetElement)
   })
 }
