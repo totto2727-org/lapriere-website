16c156f573411da53b1deef1f19b28b33edb6de0
totto2727 <kaihatu.totto2727@gmail.com>
2024-07-12 21:30:15 +0900

    fix(ci): optimize playwright snapshot path

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ expect.extend(matchers)
 
 export default defineConfig({
   testDir: 'src/playwright',
-  snapshotPathTemplate: './.snapshot/{testDir}/{testFilePath}/{arg}{ext}',
+  snapshotPathTemplate: '.snapshot/{testFilePath}/{arg}{ext}',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
